77b195bbff03e350b333ccf29e2ac7fc86f8db05
sontungpytn <sontungptytn@gmail.com>
2020-03-02 21:05:06 +0700

    Fix counting when some folders start with the same name

--- app/Controllers/Http/ExplorerController.js
+++ app/Controllers/Http/ExplorerController.js
@@ -610,7 +610,8 @@ class ExplorerController {
         ).length
 
         for (let file of allFiles) {
-          if (file.filepath.startsWith(dir)) {
+          const dirLength = dir.length
+          if (file.filepath.startsWith(dir) && file.filepath.length > dirLength && file.filepath[dirLength] === '\\') {
             if (file.isUnclassified) {
               dirsObject[dir].unclassified += 1
             } else {
