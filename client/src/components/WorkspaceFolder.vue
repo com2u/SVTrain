7f297c557afcac463182911ba4ed384fe3ba4b05
Zaydme <zaydme8@gmail.com>
2022-09-19 03:29:09 +0100

    Bugfix to calculate workspace progress bar

--- client/src/components/WorkspaceFolder.vue
+++ client/src/components/WorkspaceFolder.vue
@@ -1,193 +1,111 @@
 <template>
   <div class="ws-container">
-    <div
-      class="folder-label"
-      :style="indent"
-      :class="wsPath === systemConfig.root + info.path ? 'selected' : ''"
-    >
+    <div class="folder-label" :style="indent" :class="wsPath === systemConfig.root + info.path ? 'selected' : ''">
       <div class="folder-label-name" :class="!depth ? 'root-item' : ''">
         <span class="icon-wrapper expand-icon">
-          <b-spinner
-            v-if="loadingChildren"
-            small
-            label="Small Spinner"
-            class="clickable-icon"
-          ></b-spinner>
+          <b-spinner v-if="loadingChildren" small label="Small Spinner" class="clickable-icon"></b-spinner>
           <template v-else>
             <template v-if="info.hasSubFolders">
-              <b-icon
-                v-if="showChildren"
-                icon="triangle-fill"
-                class="option-icon clickable-icon"
-                @click="toggleShowChildren"
-              />
-              <b-icon
-                v-else
-                rotate="90"
-                icon="triangle-fill"
-                class="option-icon clickable-icon"
-                @click="toggleShowChildren"
-              />
+              <b-icon v-if="showChildren" icon="triangle-fill" class="option-icon clickable-icon"
+                @click="toggleShowChildren" />
+              <b-icon v-else rotate="90" icon="triangle-fill" class="option-icon clickable-icon"
+                @click="toggleShowChildren" />
             </template>
             <span v-else class="margin-keeper" />
           </template>
         </span>
-        <span
-          class="name"
-          @click="setWorkspace"
-          :style="{
-            fontSize: subFolderFontSize,
-          }"
-          >{{ info.name }}</span
-        >
+        <span class="name" @click="setWorkspace" :style="{
+          fontSize: subFolderFontSize,
+        }">{{ info.name }}</span>
       </div>
       <div class="options">
         <div class="option-progress" title="Percentage Classified">
-          <span class="file-nums option-progress-text"
-            >{{ totalFiles }} files</span
-          >
-          <span class="option-progress-text" v-if="showSubFolderProgress"
-            >{{ progress }}%</span
-          >
-          <progress
-            :max="100"
-            :value="progress"
-            class="ws-progress"
-            v-bind:class="{
-              'opacity-0': !(showSubFolderProgress || depth === 0),
-            }"
-          >
-          </progress>
+          <span class="file-nums option-progress-text">{{ totalFiles }} files</span>
+          <span class="option-progress-text" v-if="
+            showSubFolderProgress &&
+            info.name.toLowerCase() !== 'unclassified' &&
+            totalFiles > 0
+          ">
+            {{ progress }}%</span>
+          <progress :max="100" :value="progress" class="ws-progress" v-bind:class="{
+            'opacity-0':
+              !(showSubFolderProgress || depth === 0) ||
+              info.name.toLowerCase() === 'unclassified' ||
+              totalFiles <= 0,
+            'cursor-pointer':
+              totalFiles > 0 &&
+              info.name.toLowerCase() !== 'unclassified' &&
+              (showSubFolderProgress || depth === 0),
+          }" @click="
+              !(
+                !(showSubFolderProgress || depth === 0) ||
+                info.name.toLowerCase() === 'unclassified' ||
+                totalFiles <= 0
+              ) && canViewStatistics
+                ? showStatistic()
+                : null
+            "></progress>
         </div>
         <div>
           <div class="icon-wrapper" title="AI Statistic">
-            <b-icon
-              icon="bar-chart-fill"
-              :class="canViewStatistics ? 'clickable-icon' : 'gray-icon'"
-              font-scale="1.5"
-              @click="showStatistic"
-            />
+            <b-icon icon="bar-chart-fill" :class="canViewStatistics ? 'clickable-icon' : 'gray-icon'" font-scale="1.5"
+              @click="showStatistic" />
           </div>
           <div class="icon-wrapper" title="Notes">
-            <b-icon
-              :icon="info && info.highlight ? 'file-text-fill' : 'file-text'"
-              class="svg-icon clickable-icon"
-              :class="info.highlight ? 'highlight' : ''"
-              @click="showNotes"
-            />
+            <b-icon :icon="info && info.highlight ? 'file-text-fill' : 'file-text'" class="svg-icon clickable-icon"
+              :class="info.highlight ? 'highlight' : ''" @click="showNotes" />
           </div>
-          <div
-            v-if="canSeeConfusionMatrix"
-            class="icon-wrapper clickable"
-            @click="showConfusionMatrix()"
-            title="Compare Workspaces"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20px"
-              height="20px"
-              viewBox="0 0 100 103"
-              preserveAspectRatio="xMidYMid meet"
-            >
-              <g
-                transform="translate(0.000000,103.000000) scale(0.100000,-0.100000)"
-                :fill="
-                  depth === 0 && wsPath !== info.path
-                    ? wsPath === info.path
-                      ? '#FFF'
-                      : '#000'
-                    : '#999'
-                "
-                stroke="none"
-              >
-                <path
-                  d="M35 988 c-3 -7 -4 -229 -3 -493 l3 -480 473 -3 472 -2 0 495 0 495
+          <div v-if="canSeeConfusionMatrix" class="icon-wrapper clickable" @click="showConfusionMatrix()"
+            title="Compare Workspaces">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 100 103"
+              preserveAspectRatio="xMidYMid meet">
+              <g transform="translate(0.000000,103.000000) scale(0.100000,-0.100000)" :fill="
+                depth === 0 && wsPath !== info.path
+                  ? wsPath === info.path
+                    ? '#FFF'
+                    : '#000'
+                  : '#999'
+              " stroke="none">
+                <path d="M35 988 c-3 -7 -4 -229 -3 -493 l3 -480 473 -3 472 -2 0 495 0 495
                 -470 0 c-367 0 -472 -3 -475 -12z m443 -483 l-3 -446 -198 0 -197 0 -3 446 -2
                 445 203 0 202 0 -2 -445z m460 0 l-3 -446 -198 -2 -197 -2 0 448 0 447 200 0
-                200 0 -2 -445z"
-                />
-                <path
-                  d="M191 859 l-24 -30 29 -30 28 -29 -29 -30 -29 -30 29 -30 29 -30 27
+                200 0 -2 -445z" />
+                <path d="M191 859 l-24 -30 29 -30 28 -29 -29 -30 -29 -30 29 -30 29 -30 27
                 33 28 32 31 -31 32 -31 30 31 30 30 -33 29 -33 30 34 29 33 30 -31 30 -30 31
-                -30 -34 -30 -33 -27 32 c-15 18 -30 32 -33 31 -4 0 -18 -14 -31 -30z"
-                />
-                <path
-                  d="M120 470 l0 -40 160 0 160 0 0 40 0 40 -160 0 -160 0 0 -40z"
-                />
-                <path
-                  d="M120 310 l0 -40 160 0 160 0 0 40 0 40 -160 0 -160 0 0 -40z"
-                />
-                <path
-                  d="M120 150 l0 -40 160 0 160 0 0 40 0 40 -160 0 -160 0 0 -40z"
-                />
-                <path
-                  d="M768 823 l-47 -47 -18 22 -18 21 -31 -28 -32 -29 52 -51 52 -51 73
-                74 74 75 -29 30 -30 30 -46 -46z"
-                />
-                <path
-                  d="M580 470 l0 -40 155 0 155 0 0 40 0 40 -155 0 -155 0 0 -40z"
-                />
-                <path
-                  d="M580 310 l0 -40 155 0 155 0 0 40 0 40 -155 0 -155 0 0 -40z"
-                />
-                <path
-                  d="M580 150 l0 -40 155 0 155 0 0 40 0 40 -155 0 -155 0 0 -40z"
-                />
+                -30 -34 -30 -33 -27 32 c-15 18 -30 32 -33 31 -4 0 -18 -14 -31 -30z" />
+                <path d="M120 470 l0 -40 160 0 160 0 0 40 0 40 -160 0 -160 0 0 -40z" />
+                <path d="M120 310 l0 -40 160 0 160 0 0 40 0 40 -160 0 -160 0 0 -40z" />
+                <path d="M120 150 l0 -40 160 0 160 0 0 40 0 40 -160 0 -160 0 0 -40z" />
+                <path d="M768 823 l-47 -47 -18 22 -18 21 -31 -28 -32 -29 52 -51 52 -51 73
+                74 74 75 -29 30 -30 30 -46 -46z" />
+                <path d="M580 470 l0 -40 155 0 155 0 0 40 0 40 -155 0 -155 0 0 -40z" />
+                <path d="M580 310 l0 -40 155 0 155 0 0 40 0 40 -155 0 -155 0 0 -40z" />
+                <path d="M580 150 l0 -40 155 0 155 0 0 40 0 40 -155 0 -155 0 0 -40z" />
               </g>
             </svg>
           </div>
-          <div
-            v-if="canSyncDB"
-            class="icon-wrapper"
-            title="Convert to Database"
-          >
-            <b-icon
-              :animation="DBSyncing ? 'fade' : undefined"
-              :class="info.isDB ? 'gray-icon' : 'clickable-icon'"
-              icon="shuffle"
-              font-scale="1.5"
-              @click="syncDB"
-            />
+          <div v-if="canSyncDB" class="icon-wrapper" title="Convert to Database">
+            <b-icon :animation="DBSyncing ? 'fade' : undefined" :class="info.isDB ? 'gray-icon' : 'clickable-icon'"
+              icon="shuffle" font-scale="1.5" @click="syncDB" />
           </div>
           <div class="icon-wrapper" title="Workspace Settings">
-            <b-icon
-              :class="
-                info.config && canEditConfig ? 'clickable-icon' : 'gray-icon'
-              "
-              icon="gear-fill"
-              font-scale="1.5"
-              @click="showConfig"
-            />
+            <b-icon :class="
+              info.config && canEditConfig ? 'clickable-icon' : 'gray-icon'
+            " icon="gear-fill" font-scale="1.5" @click="showConfig" />
           </div>
           <div v-if="canBackup" class="icon-wrapper" title="Backup Workspace">
-            <b-icon
-              class="clickable-icon"
-              icon="server"
-              font-scale="1.5"
-              @click="backup()"
-            />
+            <b-icon class="clickable-icon" icon="server" font-scale="1.5" @click="backup()" />
           </div>
         </div>
       </div>
     </div>
     <div v-if="hasChildren && showChildren">
-      <workspace-folder
-        v-for="(folder, index) in info.subFolders"
-        :rawInfo="folder"
-        :depth="depth + 1"
-        :key-path="`${keyPath}.subFolders.[${index}]`"
-        :key="folder.path"
-        :show-sub-folder-progress="showSubFolderProgress"
-        @select-workspace="$emit('select-workspace', $event)"
-      />
-      <div
-        class="new-ws"
-        :style="{
-          marginLeft: `${(depth + 1) * 50}px`,
-        }"
-        v-if="systemConfig.newWorkspace && newWorkspace"
-        @click="createNewFolder"
-      >
+      <workspace-folder v-for="(folder, index) in info.subFolders" :rawInfo="folder" :depth="depth + 1"
+        :key-path="`${keyPath}.subFolders.[${index}]`" :key="folder.path"
+        :show-sub-folder-progress="showSubFolderProgress" @select-workspace="$emit('select-workspace', $event)" />
+      <div class="new-ws" :style="{
+        marginLeft: `${(depth + 1) * 50}px`,
+      }" v-if="systemConfig.newWorkspace && newWorkspace" @click="createNewFolder">
         <b-icon icon="plus-circle"></b-icon>
         New Workspace
       </div>
@@ -234,7 +152,7 @@ export default {
       return this.$store.state.app.config
     },
     totalFiles() {
-      return (this.info.unclassified + this.info.classified).toLocaleString()
+      return (this.info.matched + this.info.mismatched).toLocaleString()
     },
     indent() {
       return { marginLeft: `${this.depth * 50}px` }
@@ -252,11 +170,11 @@ export default {
       )
     },
     progress() {
-      if (this.info.matched !== undefined) {
-        if (!this.info.mismatched) return 100
+      if (this.info.classified !== undefined) {
+        if (!this.info.unclassified) return 100
         return +(
-          (this.info.matched / // eslint-disable-line operator-linebreak
-            (this.info.matched + this.info.mismatched)) * // eslint-disable-line operator-linebreak
+          (this.info.classified / // eslint-disable-line operator-linebreak
+            (this.info.classified + this.info.unclassified)) * // eslint-disable-line operator-linebreak
           100
         ).toFixed(1)
       }
@@ -317,7 +235,16 @@ export default {
     },
     getSubFolderStatistics(folderInfo) {
       if (folderInfo.hasSubFolders && folderInfo.subFolders) {
-        return this.sumObjectsByKey(...folderInfo.subFolders)
+        return this.sumObjectsByKey(
+          {
+            classified: folderInfo.classified,
+            unclassified: folderInfo.unclassified,
+            missed: folderInfo.missed,
+            matched: folderInfo.matched,
+            mismatched: folderInfo.mismatched,
+          },
+          ...folderInfo.subFolders,
+        )
       }
       return folderInfo
     },
@@ -397,6 +324,9 @@ export default {
           ...this.getSubFolderStatistics(currentSubFolder),
         }
         Object.entries(subFolderStats).forEach(([key, value]) => {
+          if (subFolderStats.path === '/Something4/test') {
+            console.log(key, value)
+          }
           if (!Number.isInteger(value)) return
           totalStats[key] = (totalStats[key] || 0) + value // eslint-disable-line no-param-reassign
         })
@@ -413,6 +343,7 @@ export default {
 </script>
 <style lang="scss">
 @keyframes pulse {
+
   0%,
   100% {
     opacity: 1;
@@ -430,22 +361,29 @@ export default {
 .tooltip {
   opacity: 1;
 }
+
 .icon-wrapper {
-      display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 }
+
 .ws-progress {
   -webkit-appearance: none;
-   appearance: none;
+  appearance: none;
   height: 1rem;
   border-radius: 0.25rem;
   overflow: hidden;
   width: 150px;
   background: black;
+
   &[value]::-webkit-progress-value {
     background: black;
   }
+}
+
+.cursor-pointer {
+  cursor: pointer;
 }
 </style>
