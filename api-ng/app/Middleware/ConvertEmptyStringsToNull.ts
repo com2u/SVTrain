89d2c5e90001f29933524c1937cd64d33b9df2d8
230119_sc_kpi <230119_sc_kpi@projects.klein.company>
2023-04-26 22:54:57 +0200

    SWT-721: migrate and refactor middleware convert empty to null

--- api-ng/app/Middleware/ConvertEmptyStringsToNull.ts
+++ api-ng/app/Middleware/ConvertEmptyStringsToNull.ts
@@ -1,8 +1,16 @@
+'use strict'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ConvertEmptyStringsToNull {
-  public async handle({}: HttpContextContract, next: () => Promise<void>) {
-    // code for middleware goes here. ABOVE THE NEXT CALL
+  public async handle({ request }: HttpContextContract, next: () => Promise<void>) {
+    if (Object.keys(request.body).length) {
+      Object.keys(request.body).forEach(
+        (k) => (request.body[k] = request.body[k] === '' ? null : request.body[k])
+      )
+    }
+
     await next()
   }
 }
+
+module.exports = ConvertEmptyStringsToNull
